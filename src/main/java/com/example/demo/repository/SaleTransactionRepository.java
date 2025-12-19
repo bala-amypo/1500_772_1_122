6ded25b4828222e8fa65dd28931709a41fbf7c2f
bala-amypo <bala@amypo.in>
2025-12-19 07:56:01 +0000

    User commit

--- src/main/java/com/example/demo/repository/SaleTransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.SaleTransaction;
-
-public interface SaleTransactionRepository extends JpaRepository<SaleTransaction, Long> {
-    // You can add custom query methods here if needed
-}
