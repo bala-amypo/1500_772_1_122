9de09645409d3b853e375730db172997f26c1f25
bala-amypo <bala@amypo.in>
2025-12-26 11:52:34 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,10 +1,7 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,18 +18,8 @@ public class OpenApiConfig {
                         .title("Influencer Campaign ROI Tracker API")
                         .version("1.0")
                         .description("API for tracking influencer marketing campaign performance and ROI"))
-                // JWT Security Scheme
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                                        .description("Enter JWT token")))
-                // Server for Swagger preview
                 .servers(List.of(
-                        new Server().url("https://9024.pro604cr.amypo.ai/")
+                        new Server().url("https://9024.pro604cr.amypo.ai")
                 ));
     }
 }
