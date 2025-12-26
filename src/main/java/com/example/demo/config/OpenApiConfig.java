3eaadef329c8b4d05f05ed80aa874c624b2c6181
bala-amypo <bala@amypo.in>
2025-12-26 12:12:55 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,26 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Influencer Campaign ROI Tracker API")
-                        .version("1.0")
-                        .description("API for tracking influencer marketing campaign performance and ROI"))
+                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9024.pro604cr.amypo.ai/swagger-ui/index.html
-")
+                        new Server().url("https://9136.408procr.amypo.ai/")
                 ));
-    }
+        }
 }
