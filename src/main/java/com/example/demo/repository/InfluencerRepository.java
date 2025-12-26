f846affce1536f92277bff04c5498fdf7b954e88
bala-amypo <bala@amypo.in>
2025-12-26 08:40:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/InfluencerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.entity.Influencer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface InfluencerRepository extends JpaRepository<Influencer, Long> {
-    Optional<Influencer> findBySocialHandle(String socialHandle);
-}
