f8a04a0ceb2497e2994c640924d6e241a4c6a71c
bala-amypo <bala@amypo.in>
2025-12-27 05:37:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/InfluencerController.java
+++ src/main/java/com/example/demo/controller/InfluencerController.java
@@ -4,7 +4,6 @@ import com.example.demo.model.Influencer;
 import com.example.demo.service.InfluencerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -22,22 +21,13 @@ public class InfluencerController {
         return ResponseEntity.ok(influencerService.createInfluencer(influencer));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Influencer> getInfluencer(@PathVariable Long id) {
-        return ResponseEntity.ok(influencerService.getInfluencerById(id));
-    }
-
     @GetMapping
     public ResponseEntity<List<Influencer>> getAllInfluencers() {
         return ResponseEntity.ok(influencerService.getAllInfluencers());
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Influencer> updateInfluencer(
-            @PathVariable Long id,
-            @RequestBody Influencer influencer) {
-        return ResponseEntity.ok(influencerService.updateInfluencer(id, influencer));
+    @GetMapping("/{id}")
+    public ResponseEntity<Influencer> getInfluencer(@PathVariable Long id) {
+        return ResponseEntity.ok(influencerService.getInfluencerById(id));
     }
 }
-
-
