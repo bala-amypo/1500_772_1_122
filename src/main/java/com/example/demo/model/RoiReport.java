e0927134f9be92024557bdc1ed15ed699f7c43e0
bala-amypo <bala@amypo.in>
2025-12-27 04:21:27 +0000

    User commit

--- src/main/java/com/example/demo/model/RoiReport.java
+++ src/main/java/com/example/demo/model/RoiReport.java
@@ -10,24 +10,24 @@ public class RoiReport {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    private DiscountCode discountCode;
-
     private BigDecimal totalSales;
     private Integer totalTransactions;
     private Double roiPercentage;
 
+    @ManyToOne
+    private DiscountCode discountCode;
+
     public RoiReport() {}
 
     public Long getId() { return id; }
-    public DiscountCode getDiscountCode() { return discountCode; }
     public BigDecimal getTotalSales() { return totalSales; }
     public Integer getTotalTransactions() { return totalTransactions; }
     public Double getRoiPercentage() { return roiPercentage; }
+    public DiscountCode getDiscountCode() { return discountCode; }
 
     public void setId(Long id) { this.id = id; }
-    public void setDiscountCode(DiscountCode discountCode) { this.discountCode = discountCode; }
     public void setTotalSales(BigDecimal totalSales) { this.totalSales = totalSales; }
     public void setTotalTransactions(Integer totalTransactions) { this.totalTransactions = totalTransactions; }
     public void setRoiPercentage(Double roiPercentage) { this.roiPercentage = roiPercentage; }
+    public void setDiscountCode(DiscountCode discountCode) { this.discountCode = discountCode; }
 }
